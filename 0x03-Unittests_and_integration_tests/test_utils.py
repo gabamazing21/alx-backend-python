cc1291329349e8b880435de6a299de55549f384f
gabriel <gabaamazing21@gmail.com>
2024-11-05 06:12:39 +0100

    okay

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from parameterized import parameterized
 import unittest
 from utils import access_nested_map
@@ -14,7 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2) 
     ])
     def test_access_nested_map(self, nested_map: Dict[str, Any],  path: Tuple[str, ...], expected: Any) -> None:
         """
