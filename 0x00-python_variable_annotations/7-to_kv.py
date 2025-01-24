e313c257dd14edfafd4f7bb1100483981d7a09f4
gabriel <gabaamazing21@gmail.com>
2025-01-25 00:24:24 +0100

    type annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from typing import List, Union, Tuple
 """ workig with type annotation"""
+
+from typing import List, Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
