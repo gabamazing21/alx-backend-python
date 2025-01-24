0b17fa47b2b209da6c88f92f1a96fcc647929836
gabriel <gabaamazing21@gmail.com>
2025-01-24 23:41:45 +0100

    type annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" workig with type annotation"""
+
+
+def sum(input_list: list[float]) -> float:
+    """
+    sum of float in pythong with type annotation
+    """
+    return sum(input_list)
