4f52372b148c6bef02689a8c76c23c10d43dec82
gabriel <gabaamazing21@gmail.com>
2025-01-24 23:56:24 +0100

    type annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from typing import List
 """ workig with type annotation"""
 
 
-def sum(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     sum of float in pythong with type annotation
     """
