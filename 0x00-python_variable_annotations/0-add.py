0554948b61b87c8d7e27b0bfc3894492e929aade
gabriel <gabaamazing21@gmail.com>
2025-01-24 21:11:29 +0100

    type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" workig with type annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """ workig with type annotation"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+type annotation in python
