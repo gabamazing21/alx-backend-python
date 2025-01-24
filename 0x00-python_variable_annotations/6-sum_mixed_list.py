d5f05b3a258d2989d341c82acc60e508230beb46
gabriel <gabaamazing21@gmail.com>
2025-01-25 00:08:46 +0100

    type annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@ from typing import List, Union
 """ workig with type annotation"""
 
 
-def sum_mixed_list(mixd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mixd_lst: List[Union[int, float]]) -> float:
     """
     type annotation
     """
