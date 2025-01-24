f9385c589f8813b28097a973ce8d09b1b4bd5ccb
gabriel <gabaamazing21@gmail.com>
2025-01-24 22:59:34 +0100

    type annotation

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 import math
-"""type annotation """
+"""type annotation for floor """
 
 
 def floor(n: float) -> int:
-    """type annotation """
+    """
+    Return the floor of the float n.
+
+    Args:
+        n (float): The input fload.
+
+    Return:
+        int: the floor value of n.
+    """
     return math.floor(n)
