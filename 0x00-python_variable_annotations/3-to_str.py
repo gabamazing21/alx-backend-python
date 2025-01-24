9736f45a40f410ec9a965b05ec8a74eeb21a5b3e
gabriel <gabaamazing21@gmail.com>
2025-01-24 23:02:39 +0100

    type annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" workig with type annotation"""
+
+
+def to_str(n: float) -> str:
+    """ workig with type annotation
+    conveting float to string"""
+    return str(n)
