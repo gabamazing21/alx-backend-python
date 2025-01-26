a49f51313d0c0fe870cbd2373c18e68ced7de55a
gabriel <gabaamazing21@gmail.com>
2025-01-26 01:54:56 +0100

    type annotation

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" workig with type annotation"""
+
+from typing import List, Tuple, Sequence
+from types import NoneType
+
+
+def zoom_array(lst: Sequence[int], factor: int = 2) -> List[int]:
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
